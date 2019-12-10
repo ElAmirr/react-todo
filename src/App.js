d5eaf46ff340289847e7c153988675e47079e4f0
ElAmirr <othmaniamir41gitgit config --global user.name ElAmirrgit config --global user.email othmaniamir41@gmail.com git config --global user.email othmaniamir41git>
2019-12-10 16:37:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';//imrc
+class App extends Component {
+  constructor(props){
+super(props)
+this.state={
+listTodo:[
+{"id":0, "text":"item1"},
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+{"id":1, "text":"item2"},
+{"id":2, "text":"item3"},
+
+
+
+
+
+],
+newText:''
+
+
 }
 
+
+
+  }
+
+  changeText=(e)=>{
+this.setState({newText:e.target.value})
+
+
+  }
+
+add=()=>{
+
+let newItm={"id":Math.random(), "text":this.state.newText}
+let newList=[...this.state.listTodo,newItm]
+this.setState({listTodo:newList})
+}
+
+  delete=(id)=>{
+    const list2=this.state.listTodo.filter((el)=>
+    {
+    return el.id!==id
+    }
+
+    )
+this.setState({
+listTodo:list2
+
+
+})
+}
+
+
+
+  render() { 
+    return ( 
+    
+    <section>
+<div>   { /*Add */}
+<input onChange={(e)=>this.changeText(e)} ></input>
+<span  onClick={this.add}>Add </span>
+
+
+</div>
+
+      
+          <div>   { /*List */}
+
+
+
+      {this.state.listTodo.map((el,i)=>{
+
+      return (
+      <p key={i}><span>{el.text}</span><span onClick={()=>this.delete(el.id)}>delete</span> </p>
+
+      )
+
+
+      }
+
+
+
+
+
+      )}
+
+
+
+          </div> 
+    
+    
+    </section>);
+  }
+}
+ 
 export default App;
+
